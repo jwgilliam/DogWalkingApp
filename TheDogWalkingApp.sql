3f93c0c7f7b66ca9f9886cc3994baee6fb7e5d52
jwgilliam <ggsmoustache@gmail.com>
2020-03-27 14:37:59 -0500

    first commit

---
+++ TheDogWalkingApp.sql
@@ -0,0 +1,36 @@
+﻿--SELECT o.Name AS OwnerName, n.Name As NeighborhoodName FROM Owner o
+--LEFT JOIN Neighborhood n ON n.Id = o.NeighborhoodId
+
+--SELECT o.Name AS OwnerName, n.Name As NeighborhoodName FROM Owner o
+--LEFT JOIN Neighborhood n ON n.Id = o.NeighborhoodId
+--WHERE o.Id = 1
+
+--Select wkr.[Name] FROM Walker wkr
+--ORDER BY [Name] ASC
+
+--SELECT DISTINCT d.Breed FROM Dog d
+
+--SELECT d.Name AS DogName, o.Name AS OwnerName, n.Name AS NeighborhoodName FROM Dog d
+--LEFT JOIN Owner o ON o.Id = d.OwnerId
+--LEFT JOIN Neighborhood n ON n.Id = o.NeighborhoodId
+
+--SELECT o.Name AS OwnerName, COUNT(d.OwnerId) AS NumberOfDogs FROM Owner o
+--LEFT JOIN Dog d ON d.OwnerId = o.Id
+--GROUP BY o.Name
+
+--SELECT wkr.[Name] AS WalkerName, COUNT(w.WalkerId) AS NumberOfWalks FROM Walker wkr
+--LEFT JOIN Walks w ON w.WalkerId =wkr.Id
+--GROUP BY wkr.Name
+
+--SELECT n.[Name] As Neighborhood, COUNT(wkr.[Name]) AS NumberOfWalkers FROM Neighborhood n
+--LEFT JOIN Walker wkr ON wkr.NeighborhoodId = n.Id
+--GROUP BY n.[Name]
+
+--SELECT d.[Name] from Dog d
+--LEFT JOIN Walks w ON w.DogId = d.Id
+--WHERE w.Date > '03/14/2020' and w.Date < '03/22/2020';
+
+--SELECT d.[Name] from Dog d
+--LEFT JOIN Walks w ON w.DogId = d.Id
+--GROUP BY d.[Name]
+--HAVING COUNT(w.Id) = 0
---
+++ TheDogWalkingAppDatabase.sql
@@ -0,0 +1,73 @@
+﻿--CREATE TABLE Neighborhood (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    [Name] VARCHAR(55) NOT NULL
+--);
+
+--CREATE TABLE Walker (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    [Name] VARCHAR(55) NOT NULL,
+--    NeighborhoodId INTEGER,
+--    CONSTRAINT FK_Walker_Neighborhood FOREIGN KEY(NeighborhoodId) REFERENCES Neighborhood(Id),
+--);
+
+--CREATE TABLE Walks (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    [Date] DATETIME NOT NULL,
+--    Duration INTEGER NOT NULL,
+--    WalkerId INTEGER,
+--    DogId INTEGER,
+--    CONSTRAINT FK_Walks_Walker FOREIGN KEY(WalkerId) REFERENCES Walker(Id),
+--    CONSTRAINT FK_Walks_Dog FOREIGN KEY(DogId) REFERENCES Dog(Id)
+--);
+
+--CREATE TABLE Dog (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    [Name] VARCHAR(55) NOT NULL,
+--    OwnerId INTEGER,
+--    CONSTRAINT FK_Dog_Owner FOREIGN KEY(OwnerId) REFERENCES [Owner](Id),
+--    Breed VARCHAR(55) NOT NULL,
+--    Notes VARCHAR(255) NOT NULL
+--);
+
+--CREATE TABLE Owner (
+--    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+--    [Name] VARCHAR(55) NOT NULL,
+--    [Address] VARCHAR(255) NOT NULL,
+--    NeighborhoodId INTEGER,
+--    CONSTRAINT FK_Owner_Neighborhood FOREIGN KEY(NeighborhoodId) REFERENCES Neighborhood(Id),
+--    Phone VARCHAR(55) NOT NULL
+--);
+
+--INSERT INTO Neighborhood([Name]) VALUES ('Drury Lane');
+--INSERT INTO Neighborhood([Name]) VALUES ('Townsville');
+--INSERT INTO Neighborhood([Name]) VALUES ('Middle of Nowhere');
+
+--INSERT INTO Walker([Name], NeighborhoodId) VALUES ('Dave Thomas', 1);
+--INSERT INTO Walker([Name], NeighborhoodId) VALUES ('Ray Kroc', 2);
+--INSERT INTO Walker([Name], NeighborhoodId) VALUES ('Glen Bell', 3);
+
+--INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Duchess', 1, 'Great Dane', 'Can bench press 2,000 lbs');
+--INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Bonaparte', 1, 'Bull Mastiff', 'Made of granite');
+--INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Festus', 2, 'St. Bernard', 'Deceased');
+--INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Hog', 2, 'Sharpei', 'Missing for 48 hours');
+--INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Douglas', 3, 'Beagle', 'Never heard of him');
+
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-17, 35, 1, 1);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-17, 35, 1, 2);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-17, 45, 2, 3);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-17, 45, 2, 4);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-17, 30, 3, 5);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-18, 40, 1, 1);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-18, 40, 1, 2);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-18, 35, 2, 3);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-18, 35, 2, 4);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-18, 35, 3, 5);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-19, 40, 1, 1);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-19, 40, 1, 2);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-19, 45, 2, 3);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-19, 45, 2, 4);
+--insert into walks([date], duration, walkerid, dogid) values (2020-03-19, 45, 3, 5);
+
+--INSERT INTO Owner([Name], [Address], NeighborhoodId, Phone) VALUES ('Frank Sobotka', 'The pier', 1, '123-456-7890');
+--INSERT INTO Owner([Name], [Address], NeighborhoodId, Phone) VALUES ('Avon Barksdale', 'Prison', 2, '123-456-7890');
+--INSERT INTO Owner([Name], [Address], NeighborhoodId, Phone) VALUES ('Marlo Stanfield', 'West', 3, '123-456-7890');
